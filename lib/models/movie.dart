942cfdf2ae1f56990d65007fe0afaf74ff34f58d
David Lindner Jr <davidlindnerjr@Davids-MacBook-Air.local>
2022-09-12 15:13:37 -0700

    Files, models, assets, folders

--- lib/models/movie.dart
+++ lib/models/movie.dart
@@ -1,24 +1,133 @@
 class Movie {
-  final int id, year, numOfRatings, criticsReview, metaScoreRating;
+  final int id, year, numOfRatings, criticsReview, metascoreRating;
   final double rating;
   final List<String> genre;
   final String plot, title, poster, backdrop;
   final List<Map> cast;
 
-  Movie(
-    this.id,
-    this.year,
-    this.numOfRatings,
-    this.criticsReview,
-    this.metaScoreRating,
-    this.rating,
-    this.genre,
-    this.plot,
-    this.title,
-    this.poster,
-    this.backdrop,
-    this.cast,
-  );
+  Movie({
+    required this.id,
+    required this.year,
+    required this.numOfRatings,
+    required this.criticsReview,
+    required this.metascoreRating,
+    required this.rating,
+    required this.genre,
+    required this.plot,
+    required this.title,
+    required this.poster,
+    required this.backdrop,
+    required this.cast,
+  });
 }
 
-List<Movie> movies = [];
+List<Movie> movies = [
+  Movie(
+    id: 1,
+    title: "Bloodshot",
+    year: 2020,
+    poster: "assets/images/poster_1.jpg",
+    backdrop: "assets/images/backdrop_1.jpg",
+    numOfRatings: 150212,
+    rating: 7.3,
+    criticsReview: 50,
+    metascoreRating: 76,
+    genre: ["Action", "Drama"],
+    plot:
+        "After he and his wife are murdered, marine Ray Garrison is resurrected by a team of scientists. Enhanced with nanotechnology, he becomes a superhuman, biotech killing machine - Bloodshot. As Ray first trains with fellow super-soldiers, he cannot recall anything from his former life. But when his memories flood back and he remembers the man that killed both him and his wife, he breaks out of the facility to get revenge, only to discover that there's more to the conspiracy than he thought.",
+    cast: [
+      {
+        "orginalName": "James Mangold",
+        "movieName": "Director",
+        "image": "assets/images/actor_1.png",
+      },
+      {
+        "orginalName": "Matt Damon",
+        "movieName": "Carroll",
+        "image": "assets/images/actor_2.png",
+      },
+      {
+        "orginalName": "Christian Bale",
+        "movieName": "Ken Miles",
+        "image": "assets/images/actor_3.png",
+      },
+      {
+        "orginalName": "Caitriona Balfe",
+        "movieName": "Mollie",
+        "image": "assets/images/actor_4.png",
+      },
+    ],
+  ),
+  Movie(
+    id: 2,
+    title: "Ford v Ferrari ",
+    year: 2019,
+    poster: "assets/images/poster_2.jpg",
+    backdrop: "assets/images/backdrop_2.jpg",
+    numOfRatings: 150212,
+    rating: 8.2,
+    criticsReview: 50,
+    metascoreRating: 76,
+    genre: ["Action", "Biography", "Drama"],
+    plot:
+        "American automotive designer Carroll Shelby and fearless British race car driver Ken Miles battle corporate interference, the laws of physics and their own personal demons to build a revolutionary vehicle for the Ford Motor Co. Together, they plan to compete against the race cars of Enzo Ferrari at the 24 Hours of Le Mans in France in 1966.",
+    cast: [
+      {
+        "orginalName": "James Mangold",
+        "movieName": "Director",
+        "image": "assets/images/actor_1.png",
+      },
+      {
+        "orginalName": "Matt Damon",
+        "movieName": "Carroll",
+        "image": "assets/images/actor_2.png",
+      },
+      {
+        "orginalName": "Christian Bale",
+        "movieName": "Ken Miles",
+        "image": "assets/images/actor_3.png",
+      },
+      {
+        "orginalName": "Caitriona Balfe",
+        "movieName": "Mollie",
+        "image": "assets/images/actor_4.png",
+      },
+    ],
+  ),
+  Movie(
+    id: 1,
+    title: "Onward",
+    year: 2020,
+    poster: "assets/images/poster_3.jpg",
+    backdrop: "assets/images/backdrop_3.jpg",
+    numOfRatings: 150212,
+    rating: 7.6,
+    criticsReview: 50,
+    metascoreRating: 79,
+    genre: ["Action", "Drama"],
+    plot:
+        "Teenage elf brothers Ian and Barley embark on a magical quest to spend one more day with their late father. Like any good adventure, their journey is filled with cryptic maps, impossible obstacles and unimaginable discoveries. But when dear Mom finds out her sons are missing, she teams up with the legendary manticore to bring her beloved boys back home.",
+    cast: [
+      {
+        "orginalName": "James Mangold",
+        "movieName": "Director",
+        "image": "assets/images/actor_1.png",
+      },
+      {
+        "orginalName": "Matt Damon",
+        "movieName": "Carroll",
+        "image": "assets/images/actor_2.png",
+      },
+      {
+        "orginalName": "Christian Bale",
+        "movieName": "Ken Miles",
+        "image": "assets/images/actor_3.png",
+      },
+      {
+        "orginalName": "Caitriona Balfe",
+        "movieName": "Mollie",
+        "image": "assets/images/actor_4.png",
+      },
+    ],
+  ),
+];
